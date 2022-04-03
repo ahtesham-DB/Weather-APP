a7312c4f63bc43607f5bf18e6828ec06a27e59ba
ahtesham-DB <102810299+ahtesham-DB@users.noreply.github.com>
2022-04-03 22:23:11 +0530

    Add files via upload

--- client-frontend/src/components/HomePage.js
+++ client-frontend/src/components/HomePage.js
@@ -2,14 +2,27 @@ import React, { useEffect, useState, useRef } from "react";
 import "./homepage.css";
 import axios from "axios";
 import Autocomplete from "react-google-autocomplete";
+import utils from "../utils/countryCodes.json";
 
 //For test and Developemt Please Use Env File to store API KEYS  URLS
 const baseURL = "https://weather-app-db-new.herokuapp.com/postWeatherApi";
 
 //For test and Developemt Please Use Env File to store API KEYS
 
-
 function HomePage() {
+  const [city, setCity] = useState(null);
+  const [inputValue, setInputValue] = useState(null);
+  const [searchTarm, setSearchTarm] = useState("city");
+  const [cuntryCode, setCuntryCode] = useState(null);
+
+  const [weatherData, setWatherData] = useState({
+    weatherFahrenheit: 85.98,
+    weatherCelsiusL: "29.99",
+    weatherDescription: "smoke",
+    country: "IN",
+    city: "Mumbai",
+  });
+
   let notificationBar = useRef(null);
 
   const searchBtnHandler = (e) => {
@@ -20,18 +33,17 @@ function HomePage() {
       return;
     }
 
-
     axios
       .post(baseURL, {
         data:
           searchTarm === "city"
             ? `${searchTarm}_${city}`
-            : `${searchTarm}_${inputValue}`,
+            : `${searchTarm}_${inputValue}_${cuntryCode}`,
       })
       .then((response) => {
         // console.log(inputValue);
         setWatherData(response.data);
-        console.log(response.status);
+        // console.log(response.status);
         if (response.status === 201) {
           // console.log(response.status);
 
@@ -41,7 +53,7 @@ function HomePage() {
         // console.log("homepage data res", response);
       })
       .catch(function (error) {
-        console.log(error);
+        // console.log(error);
         if (error.response.status > 300) {
           notificationBar.current.style.display = "block";
           notificationBar.current.innerHTML =
@@ -52,79 +64,72 @@ function HomePage() {
   const onChangeHandler = (e) => {
     setInputValue(e.target.value);
   };
-  const [city, setCity] = useState(null);
-  const [inputValue, setInputValue] = useState(null);
-  const [searchTarm, setSearchTarm] = useState("city");
-  const [weatherData, setWatherData] = useState({
-    weatherFahrenheit: 85.98,
-    weatherCelsiusL: "29.99",
-    weatherDescription: "smoke",
-    country: "IN",
-    city: "Mumbai",
-  });
 
-  const setSearchType = (event) => {
-    setSearchTarm(event.target.value);
+  const setCuntryCodeHandler = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setCuntryCode(event.target.value.toLowerCase());
+    // console.log(cuntryCode);
+  };
+
+  const setSearchType = (et) => {
+    et.preventDefault();
+    setSearchTarm(et.target.value);
   };
 
   useEffect(() => {
-    if('geolocation' in navigator){
+    if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(setLatLong, showError);
-    }else{
+    } else {
       notificationBar.current.style.display = "block";
-      notificationBar.current.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+      notificationBar.current.innerHTML =
+        "<p>Browser doesn't Support Geolocation</p>";
     }
-    
-    
+
     // SET USER'S POSITION
     function setLatLong(position) {
-  
-      console.log("lll")
+      // console.log("lll");
       let latitude = position.coords.latitude;
       let longitude = position.coords.longitude;
       // let api = `${baseURL}${latitude},${longitude}`
-      let api = `https://weather-app-db-new.herokuapp.com/get`
-      // let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0e8653259a8d5a9634be266951e936ce`;
-      console.log(latitude+" " +longitude)
+      let api = `https://weather-app-db-new.herokuapp.com/get`;
+      // let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={id here};
+      console.log(latitude + " " + longitude);
       axios
-      .get(api, { params: { lat: latitude, long:longitude
-      }})
-      .then((response) => {
-        // console.log(inputValue);
-        // setWatherData(response.data);
-        console.log(response.status);
-        console.log(response);
-        if(response.status === 200){
-          
-                  let weatherTemp = response.data.main.temp - 273
-                  let weatherName = response.data.name
-                  let weathercountry = response.data.sys.country
-                  let weatherdec= response.data.weather[0].description
-        
-                  let data = {
-                    weatherFahrenheit: 85.98,
-                    weatherCelsiusL: Math.floor(weatherTemp),
-                    weatherDescription: weatherdec,
-                    country: weathercountry,
-                    city: weatherName,
-                  }
-                  console.log(weatherTemp+weatherName+weathercountry+weatherdec)
-                  setWatherData(data)
-          // if (response.data.weather.main == undefined) return 
+        .get(api, { params: { lat: latitude, long: longitude } })
+        .then((response) => {
+          // console.log(inputValue);
+          // setWatherData(response.data);
+          console.log(response.status);
+          console.log(response);
+          if (response.status === 200) {
+            let weatherTemp = response.data.main.temp - 273;
+            let weatherName = response.data.name;
+            let weathercountry = response.data.sys.country;
+            let weatherdec = response.data.weather[0].description;
 
+            let data = {
+              weatherFahrenheit: 85.98,
+              weatherCelsiusL: Math.floor(weatherTemp),
+              weatherDescription: weatherdec,
+              country: weathercountry,
+              city: weatherName,
+            };
+            console.log(
+              weatherTemp + weatherName + weathercountry + weatherdec
+            );
+            setWatherData(data);
+            // if (response.data.weather.main == undefined) return
           }
-        
-
+        })
+        .catch(function (error) {
   
-      })
-      .catch(function (error) {
-        console.log(error);
-        if (error.response.status > 300) {
-          notificationBar.current.style.display = "block";
-          notificationBar.current.innerHTML =
-            "<p>Please enter correct City Name Or City ZipCode</p>";
-        }
-      });
+          if (error.response.status > 300) {
+            notificationBar.current.style.display = "block";
+            notificationBar.current.innerHTML =
+              "<p>Please enter correct City Name Or City ZipCode</p>";
+          }
+        });
 
       // getWeather(latitude, longitude);
       // console.log(latitude + longitude);
@@ -135,7 +140,7 @@ function HomePage() {
       notificationBar.current.style.display = "block";
       notificationBar.current.innerHTML = `<p> ${error.message} </p>`;
     }
-  },[])
+  }, []);
   // useEffect(() => {
   //   axios
   //     .post(baseURL, {
@@ -162,19 +167,17 @@ function HomePage() {
         type="number/text"
         onChange={onChangeHandler}
         onPlaceSelected={(place) => {
-          console.log(place);
           setCity(place.address_components[0].long_name);
         }}
       />
-
-      <button className="btnSearch" onClick={searchBtnHandler}>
-        Search
-      </button>
-
       <div className="containerCheckBoxs">
-        <div className="selector" onChange={setSearchType}>
-          <div className="selecotr-item">
-            <label htmlFor="radio1" value="city" className="selector-item_label">
+        <div className="selector">
+          <div className="selecotr-item" onChange={setSearchType}>
+            <label
+              htmlFor="radio1"
+              value="city"
+              className="selector-item_label"
+            >
               City Name
             </label>
             <input
@@ -184,7 +187,11 @@ function HomePage() {
               name="selector"
               className="selector-item_radio"
             />
-            <label htmlFor="radio2" value="zipcode" className="selector-item_label">
+            <label
+              htmlFor="radio2"
+              value="zipcode"
+              className="selector-item_label"
+            >
               City Zip Code
             </label>
             <input
@@ -194,9 +201,19 @@ function HomePage() {
               name="selector"
               className="selector-item_radio"
             />
+            <div></div>
+            <select onChange={setCuntryCodeHandler}>
+              {utils.map((item, index) => (
+                <option key={index}> {item.Code}</option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
+
+      <button className="btnSearch" onClick={searchBtnHandler}>
+        Search
+      </button>
 
       <div className="container">
         <div className="app-title">
