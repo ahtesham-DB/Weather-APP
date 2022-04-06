97aba4b87add9cb97e9d3dc94d39a0dde8180e86
ahtesham-DB <102810299+ahtesham-DB@users.noreply.github.com>
2022-04-06 11:15:20 +0530

    Update index.js

--- server-backend/index.js
+++ server-backend/index.js
@@ -6,15 +6,22 @@ import "dotenv/config";
 
 import router from "./routes/weatherRoutes.js";
 
+//Env config File to store Secret Data API Keys Connection URL
+
+
+//Set-up local 3001 ports, but if deployed on Server then Hosting service will provide Open PORT
 const PORT = process.env.PORT || 3001;
 
+// iniciate Expresss server 
 const app = express();
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(cors());
 
+//setting Routs in saprat modules 
 app.use("/", router);
 
+//  Initiate mongoDB connection and passing appropriate options 
 export const conn = mongoose
   .connect(process.env.MONGODBURL, {
     useNewUrlParser: true,
@@ -29,4 +36,6 @@ export const conn = mongoose
     console.log(err);
   });
 
+
+//Deprecated
 // mongoose.set('useFindAndModify', false);
