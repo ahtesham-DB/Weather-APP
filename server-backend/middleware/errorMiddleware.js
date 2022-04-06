f507be85e8fe11a081db6c224415b178de17c439
ahtesham-DB <102810299+ahtesham-DB@users.noreply.github.com>
2022-04-06 11:19:20 +0530

    Update errorMiddleware.js

--- server-backend/middleware/errorMiddleware.js
+++ server-backend/middleware/errorMiddleware.js
@@ -1,9 +1,11 @@
+//Middleware to handle a bad request, not found URLS
 export const notFound = (req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
   };
   
+//Middleware to handle a error,
   export const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
